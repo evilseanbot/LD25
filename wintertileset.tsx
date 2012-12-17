301a7c7b619cc85c3d3478c216489d623d567df4
Sean Musgrave <seanmusgraveit@gmail.com>
2012-12-16 23:35:00 -0800

    Made night tileset

---
+++ wintertileset.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="winterhouse_tileset" tilewidth="64" tileheight="64">
+ <image source="tileset.png" width="320" height="320"/>
+ <tile id="0">
+  <properties>
+   <property name="components" value="Solid"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="components" value="Solid"/>
+  </properties>
+ </tile>
+</tileset>
